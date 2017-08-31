daa27846392a0313caa2d78217b031c72c8d76ff
ArthurGurinovich <gurinovichag@gmail.com>
2017-08-31 09:49:29 +0300

    Connect React and React-DOM

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Dropdown from './Dropdown';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dropdown/>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
